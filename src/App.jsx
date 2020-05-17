79eac9b9f3cdabb4a542faba04243866056cb1aa
soumyadeepsinha <s.sinha2701@gmail.com>
2020-05-17 19:51:47 +0530

    Bug Fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Logic from "./Components/Logic/Logic";
 
 function App() {
   // creating a name variable
@@ -11,10 +12,10 @@ function App() {
   curHours = curHours.getHours();
   curMin = curMin.getMinutes();
 
-  let AmOrPm = curHours >= 12 ? 'pm' : 'am';
-  curHours = (curHours % 12) || 12;
+  let AmOrPm = curHours >= 12 ? "pm" : "am";
+  let curTime = curHours % 12 || 12;
 
-  let finalTime = "Time - " + curHours + ":" + curMin + " " + AmOrPm;
+  let finalTime = "Time - " + curTime + ":" + curMin + " " + AmOrPm;
 
   // creating a greetigs variable
   let greetings = {};
@@ -22,13 +23,13 @@ function App() {
   // creating a inline-css variable
   const cssstyle = {};
 
-  if (curHours >= 4 && curHours <= 11 && curMin <= 59 && AmOrPm === 'am') {
+  if (curHours >= 4 && curHours <= 11) {
     greetings = "Good Morning";
     cssstyle.color = "green";
-  } else if (curHours >= 12 && curHours <= 5 && curMin <= 59 && AmOrPm === 'pm') {
+  } else if (curHours >= 12 && curHours <= 17) {
     greetings = "Good Afternoon";
     cssstyle.color = "orange";
-  } else if (curHours >= 6 && curHours <= 7 && curMin <= 59 && AmOrPm === 'pm') {
+  } else if (curHours >= 18 && curHours <= 19) {
     greetings = "Good Evening";
     cssstyle.color = "white";
   } else {
@@ -38,9 +39,8 @@ function App() {
   return (
     <>
       <div className="container">
-        <h1 className="clock">
-          {finalTime}
-        </h1>
+        <Logic />
+        <h1 className="clock">{finalTime}</h1>
         <h2>
           <span style={cssstyle}> {greetings}, </span> {name}
         </h2>
---
+++ src/Components/Logic/Logic.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './Logic.modules.css';
+
+export class Logic extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome to React App...</h1>
+        <p className="made_by">Made by Soumyadeep Sinha</p>
+      </div>
+    )
+  }
+}
+
+export default Logic
